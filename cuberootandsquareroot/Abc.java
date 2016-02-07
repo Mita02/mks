f5c421537ee35c1e28db900ba228419d38925c9a
Mita02 <sawantmitali22@gmail.com>
2016-02-07 20:12:27 +0530

    1st commit

---
+++ cuberootandsquareroot/Abc.java
@@ -0,0 +1,15 @@
+package abc;
+public class Abc 
+{
+    public static void main(String[] args) 
+    {
+        int a=9;
+        double b=27.453;
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.cbrt(b));
+       
+        
+        
+    }
+    
+}

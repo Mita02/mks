d76ed22dec5efd976527d782e23f5b8e7f274911
Mita02 <sawantmitali22@gmail.com>
2016-02-08 19:50:09 +0530

    assignment 1.4

---
+++ SolvingExpression/JavaApplication8.java
@@ -0,0 +1,22 @@
+package javaapplication8;
+public class JavaApplication8 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        
+        int x=5,y=10,a,b,c,d;
+        a=x+y*2;
+        System.out.println(a);
+        b=x-y+2;
+        System.out.println(b);
+        c=(x+y)*2;
+        System.out.println(c);
+        d=y%x;
+        System.out.println(d);
+        
+        
+    }
+    
+}
